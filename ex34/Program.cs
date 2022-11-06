a9e2a6a992f31cd25b932a540504f16525a1cf2b
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-06 20:16:25 +0700

    feat: add solution of task 34

---
+++ ex34/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] fillArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for(int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+int CountEven(int [] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+            count++;    
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов массива");
+int length = Convert.ToInt32 (Console.ReadLine());
+int[] newArray = fillArray(length);
+Console.WriteLine($"Количество четных элементов в массиве {CountEven(newArray)}");

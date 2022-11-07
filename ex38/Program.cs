3a5d6d756ca931d441aaf6f4610ccbf24bd4c958
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-07 12:00:46 +0700

    feat: add solution of task 38

---
+++ ex38/Program.cs
@@ -0,0 +1,31 @@
+﻿double[] fillArrayDouble(int lenght)
+{
+    double[] array = new double[lenght];
+    for(int i = 0; i < lenght; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * 100, 2);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+double DiffBetweenMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+        else if(array[i] < min)
+            min = array[i];
+    }
+    double result = max - min;
+    return result;
+}
+
+Console.WriteLine("Введите количество элементов массива");
+int length = Convert.ToInt32 (Console.ReadLine());
+double[] newArray = fillArrayDouble(length);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива {DiffBetweenMaxMin(newArray)}");
